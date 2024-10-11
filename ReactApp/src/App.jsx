71a1ff7343b7a9ae88175f81e7e88a26a2a13bce
@AnjaliPrasannan <anjali.prasannan@axelerant.com>
2024-10-11 12:56:09 +0530

    User Card component - Basic Component

--- ReactApp/src/App.jsx
+++ ReactApp/src/App.jsx
@@ -1,5 +1,24 @@
-function App() {
-  return "Hi";
-}
+import "./user.css";
+import user from "./user.json";
+import { UserCard } from "./UserCard";
+import { UserCardClass } from "./UserCardClass";
 
-export default App;
+export default function App() {
+  return (
+    <div>
+      <UserCard
+        name={user.name}
+        phoneNumber={user.phoneNumber}
+        age={user.age}
+        address={user.address}
+      />
+      <br />
+      <UserCardClass
+        name={user.name}
+        phoneNumber={user.phoneNumber}
+        age={user.age}
+        address={user.address}
+      />
+    </div>
+  );
+}
