ff3440042afb85d70f7329305a5d834bcd5473bf
@AnjaliPrasannan <anjali.prasannan@axelerant.com>
2024-10-15 16:18:03 +0530

    User List - 38

--- ReactApp/src/App.jsx
+++ ReactApp/src/App.jsx
@@ -1,5 +1,55 @@
-function App() {
-  return "Hi";
-}
+import { useEffect, useState } from "react";
+import { User } from "./User";
 
-export default App;
+export default function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    const controller = new AbortController();
+    fetch("https://jsonplaceholder.typicode.com/users", {
+      signal: controller.signal,
+    })
+      .then((res) => {
+        if (res.status == 200) {
+          return res.json();
+        } else {
+          return Promise.reject(res);
+        }
+      })
+      .then((data) => {
+        console.log("Here");
+        console.log(loading);
+        setUsers(data);
+      })
+      .catch((e) => {
+        if (e?.name === "AbortError") return;
+        setError(e);
+      })
+
+      .finally(() => {
+        setLoading(false);
+      });
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
+  return (
+    <>
+      <h1>User List</h1>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <ul>
+          {users.map((user) => {
+            return <User key={user.id} name={user.name} />;
+          })}
+        </ul>
+      )}
+    </>
+  );
+}
